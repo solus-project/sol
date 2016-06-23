b7fda1c3dd1a9140e2c869a2ba045bee4c0ba580
Ikey Doherty <ikey@solus-project.com>
2016-06-23 02:29:00 +0100

    Add initial dumb frontend to get things in place
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/cli/main.c
@@ -0,0 +1,17 @@
+/*
+ * This file is part of eopkg.
+ *
+ * Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+ *
+ * eopkg is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+        return EXIT_SUCCESS;
+}
