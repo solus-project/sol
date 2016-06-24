f61f4305e5ce335e50e25cc08678e6cf76f2c8f1
Ikey Doherty <ikey@solus-project.com>
2016-06-24 15:23:59 +0100

    Document the API
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/lib/eopkg-atomics.h
+++ src/lib/eopkg-atomics.h
@@ -22,11 +22,24 @@
  */
 typedef void (*eopkg_atomic_free)(void *);
 
+/**
+ * Base type for atomic reference types
+ *
+ * Ensure the eopkg_atomic_t is the first member in the struct to gain
+ * "free" atomic refcounts. Always proxy by eopkg_atomic_init after
+ * construction
+ */
 typedef struct eopkg_atomic_t {
-        atomic_int ref_count;
-        eopkg_atomic_free dtor;
+        atomic_int ref_count;   /**<Current ref count */
+        eopkg_atomic_free dtor; /**<Deconstructor when ref count hits 0 */
 } eopkg_atomic_t;
 
+/**
+ * Increase the refcount of a given object by one, atomically.
+ *
+ * @note: @v is assumed to be an eopkg_atomic_t based struct
+ * @returns a reference to the ref'd struct
+ */
 __eopkg_inline__ static inline void *eopkg_atomic_ref(void *v)
 {
         eopkg_atomic_t *t = v;
@@ -38,6 +51,12 @@ __eopkg_inline__ static inline void *eopkg_atomic_ref(void *v)
         return t;
 }
 
+/**
+ * Initialse an atomic reference type to a refcount of 1
+ *
+ * @param dtor Deconstructor to call when the refcount hits 0
+ * @returns A reference to the object
+ */
 __eopkg_inline__ static inline void *eopkg_atomic_init(eopkg_atomic_t *atom, eopkg_atomic_free dtor)
 {
         assert(atom->dtor == NULL);
@@ -46,6 +65,13 @@ __eopkg_inline__ static inline void *eopkg_atomic_init(eopkg_atomic_t *atom, eop
         return eopkg_atomic_ref(atom);
 }
 
+/**
+ * Decrease the refcount for an object by 1.
+ * @note When the refcount hits 0, the deconstructor is called. If this doesn't
+ * exist then it is assumed to be a basic type, and @free is called.
+ *
+ * @returns A reference to the object if the refcount is higher than 0, or NULL
+ */
 __eopkg_inline__ static inline void *eopkg_atomic_unref(void *v)
 {
         eopkg_atomic_t *t = v;
