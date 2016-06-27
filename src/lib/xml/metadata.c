4fefe569d9efa48c951acc2b13cc2e72fbd469d7
Ikey Doherty <ikey@solus-project.com>
2016-06-27 17:02:15 +0100

    Stick in the boilerplate to start parsing XML
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/lib/xml/metadata.c
+++ src/lib/xml/metadata.c
@@ -9,12 +9,37 @@
  * of the License, or (at your option) any later version.
  */
 
-#include "metadata.h"
+#include <libxml/SAX2.h>
+#include <libxml/parser.h>
+#include <libxml/parserInternals.h>
+
 #include "eopkg-atomics.h"
+#include "metadata.h"
 
 struct EopkgMetadata {
         eopkg_atomic_t eatom;
 };
+
+/**
+ * Handle opening of a new element
+ */
+static void eopkg_sax_start_element(void *udata, const xmlChar *name, const xmlChar **attrs)
+{
+}
+
+/**
+ * Handle closing of an element
+ */
+static void eopkg_sax_end_element(void *udata, const xmlChar *name)
+{
+}
+
+/**
+ * Handle character input
+ */
+static void eopkg_sax_characters(void *udata, const xmlChar *ch, int len)
+{
+}
 
 static void eopkg_metadata_free(EopkgMetadata *self)
 {
@@ -37,8 +62,36 @@ EopkgMetadata *eopkg_metadata_unref(EopkgMetadata *meta)
         return eopkg_atomic_unref(meta);
 }
 
-bool eopkg_metadata_load(EopkgMetadata *meta, __eopkg_unused__ const char *filename)
+bool eopkg_metadata_load(EopkgMetadata *meta, const char *filename)
 {
         assert(meta != NULL);
-        return false;
+        xmlParserCtxtPtr ctx = NULL;
+        xmlDocPtr p = NULL;
+        xmlSAXHandler *old = NULL;
+        xmlSAXHandler handler = {.startElement = eopkg_sax_start_element,
+                                 .endElement = eopkg_sax_end_element,
+                                 .characters = eopkg_sax_characters };
+        ctx = xmlCreateFileParserCtxt(filename);
+        if (!ctx) {
+                fprintf(stderr, "eopkg: Error creating XML context\n");
+                return false;
+        }
+        old = ctx->sax;
+        ctx->sax = &handler;
+        /* ctx->userData = &instance; */
+        ctx->userData = NULL;
+
+        xmlParseDocument(ctx);
+        p = ctx->myDoc;
+        bool good = ctx->wellFormed;
+        ctx->sax = old;
+        xmlFreeParserCtxt(ctx);
+        if (p) {
+                xmlFreeDoc(p);
+        }
+
+        if (!good) {
+                fprintf(stderr, "eopkg: Badly formed XML file, aborting\n");
+        }
+        return good;
 }
